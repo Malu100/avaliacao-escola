32dc59289616ddae52c007adc0e0187241595735
WillianPiedade08 <willianpiedade17@gmail.com>
2025-10-21 08:27:31 -0300

    .

--- docs/banco.sql
+++ docs/banco.sql
@@ -1,4 +1,3 @@
--- Criação das tabelas
 CREATE TABLE IF NOT EXISTS professores (
     id INTEGER PRIMARY KEY,
     email TEXT NOT NULL,
@@ -20,18 +19,15 @@ CREATE TABLE IF NOT EXISTS atividades (
     FOREIGN KEY(turma_id) REFERENCES turmas(id)
 );
 
--- Inserção de dados de teste
--- Professores
-INSERT OR IGNORE INTO professores (email, senha, nome) VALUES ('prof1@example.com', 'senha1', 'Prof A');
-INSERT OR IGNORE INTO professores (email, senha, nome) VALUES ('prof2@example.com', 'senha2', 'Prof B');
-INSERT OR IGNORE INTO professores (email, senha, nome) VALUES ('prof3@example.com', 'senha3', 'Prof C');
+-
+INSERT OR IGNORE INTO professores (email, senha, nome) VALUES ('prof_joao@gmail.com', 'senai123', 'João');
+INSERT OR IGNORE INTO professores (email, senha, nome) VALUES ('prof_rebeca@gmail.com', 'senai112', 'Rebeca');
+INSERT OR IGNORE INTO professores (email, senha, nome) VALUES ('prof_gabriel@gmail.com', 'senai122', 'Gabriel');
 
--- Turmas (associadas ao professor com id=1)
 INSERT OR IGNORE INTO turmas (nome, professor_id) VALUES ('Turma 1A', 1);
 INSERT OR IGNORE INTO turmas (nome, professor_id) VALUES ('Turma 1B', 1);
 INSERT OR IGNORE INTO turmas (nome, professor_id) VALUES ('Turma 1C', 1);
 
--- Atividades (associadas à turma com id=1)
 INSERT OR IGNORE INTO atividades (descricao, turma_id) VALUES ('Atividade 1', 1);
 INSERT OR IGNORE INTO atividades (descricao, turma_id) VALUES ('Atividade 2', 1);
 INSERT OR IGNORE INTO atividades (descricao, turma_id) VALUES ('Atividade 3', 1);
